2a5a61c4c2800d8dde3da532811e889a2382e9bd
fridamsh <fridas@stud.ntnu.no>
2018-02-20 18:40:54 +0100

    Variable shows login status

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
 	<div>
 		<?php
 			if (isset($_SESSION['u_id'])) {
-				echo "You are logged in!";
+				$status = "You are logged in :D";
+			} else {
+				$status = "You are not logged in :(";
 			}
 		?>
+		<p><?php echo $status ?></p>
 	</div>
 </section>
