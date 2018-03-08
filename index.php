ea947adb5afa65d6cb2fb789a7d044b24508e074
fridamsh <fridas@stud.ntnu.no>
2018-03-08 16:22:04 +0100

    Mekka logikk for å vise og fjerne turene fra kartet

--- index.php
+++ index.php
@@ -158,7 +158,47 @@
 
 	var overlays = {};
 
+	var layer = L.layerGroup().addTo(mymap);
+
 	var sidebar = L.control.sidebar('sidebar').addTo(mymap);
+
+	var redIcon = L.icon({
+	    iconUrl: 'img/marker-icon-2x-red-2.png',
+	    iconSize: [25, 41],
+	    iconAnchor: [13, 40],
+	    popupAnchor: [0, -33],
+	    shadowUrl: 'img/marker-shadow.png',
+	    shadowSize: [41, 41],
+	    shadowAnchor: [13, 40]
+	});
+	var blueIcon = L.icon({
+	    iconUrl: 'img/marker-icon-2x-blue.png',
+	    iconSize: [25, 41],
+	    iconAnchor: [13, 40],
+	    popupAnchor: [0, -33],
+	    shadowUrl: 'img/marker-shadow.png',
+	    shadowSize: [41, 41],
+	    shadowAnchor: [13, 40]
+	});
+
+	var startIcon = L.icon({
+	    iconUrl: 'img/marker-icon-start.png',
+	    iconSize: [20, 20],
+	    iconAnchor: [10, 10],
+	    popupAnchor: [0, -5],
+	    shadowUrl: 'img/shadow.png',
+	    shadowSize: [20, 20],
+	    shadowAnchor: [7, 11]
+	});
+	var stopIcon = L.icon({
+	    iconUrl: 'img/marker-icon-stop.png',
+	    iconSize: [20, 20],
+	    iconAnchor: [10, 10],
+	    popupAnchor: [0, -5],
+	    shadowUrl: 'img/shadow.png',
+	    shadowSize: [20, 20],
+	    shadowAnchor: [7, 11]
+	});
 
     function reqListener () {
       console.log(this.responseText);
@@ -188,44 +228,8 @@
 		    	var observationPoints = obj[k].observationPoints;
 		    	var track = obj[k].track;
 
-		    	var startIcon = L.icon({
-				    iconUrl: 'img/marker-icon-start.png',
-				    iconSize: [20, 20],
-				    iconAnchor: [10, 10],
-				    popupAnchor: [0, -5],
-				    shadowUrl: 'img/shadow.png',
-				    shadowSize: [20, 20],
-				    shadowAnchor: [7, 11]
-				});
-				var stopIcon = L.icon({
-				    iconUrl: 'img/marker-icon-stop.png',
-				    iconSize: [20, 20],
-				    iconAnchor: [10, 10],
-				    popupAnchor: [0, -5],
-				    shadowUrl: 'img/shadow.png',
-				    shadowSize: [20, 20],
-				    shadowAnchor: [7, 11]
-				});
+		    	//Make a list for the map items as markers and polylines
 				mapItems = [];
-
-				var redIcon = L.icon({
-				    iconUrl: 'img/marker-icon-2x-red-2.png',
-				    iconSize: [25, 41],
-				    iconAnchor: [13, 40],
-				    popupAnchor: [0, -33],
-				    shadowUrl: 'img/marker-shadow.png',
-				    shadowSize: [41, 41],
-				    shadowAnchor: [13, 40]
-				});
-				var blueIcon = L.icon({
-				    iconUrl: 'img/marker-icon-2x-blue.png',
-				    iconSize: [25, 41],
-				    iconAnchor: [13, 40],
-				    popupAnchor: [0, -33],
-				    shadowUrl: 'img/marker-shadow.png',
-				    shadowSize: [41, 41],
-				    shadowAnchor: [13, 40]
-				});
 
 				//Decode observation points
 				var jsonObservationPoints = JSON.parse(observationPoints);
@@ -283,15 +287,12 @@
 				    smoothFactor: 1
 				}).bindPopup('<b>'+title+'</b><br>'+dateStart.format("dd/mm/yyyy HH:MM")+'-'+dateEnd.format('HH:MM')+'<br><b>Gjeter:</b> '+name+'<br><b>Deltakere:</b> '+participants+'<br><b>Antall sau sett:</b> '+totalSheepCount+'<br><b>Vær:</b> '+weather+'<br><b>Distanse:</b> '+distance+'<br><b>Detaljer:</b> '+description);
 				mapItems.push(trackPolyline);
-				mymap.fitBounds(trackPolyline.getBounds());
+				//mymap.fitBounds(trackPolyline.getBounds());
 
 				//Add all map items to the map
-				var layer = L.layerGroup(mapItems).addTo(mymap);
-				var key = "layer"+k;
-				overlays.key=layer;
+				//var layer = L.layerGroup(mapItems).addTo(mymap);
 
 				//Add hikes to dates page
-				//var newHTML = '<a>'+title+' '+dateStart.format("dd/mm/yyyy HH:MM")+'</a><br>';
 				var newHTML = '<li><input type="checkbox" id="'+id+'"/> '+title+' '+dateStart.format("dd/mm/yyyy HH:MM")+'</li>';
 				$("#hikes-list").append(newHTML);
 	    	}
@@ -301,28 +302,122 @@
     oReq.send();
 
     $('#hikes-list').on('change', 'input[type="checkbox"]', function() {
-	    //alert('click registered');
+	    var hikeId = $(this).attr('id');
 	    if (this.checked) {
-	    	// Show the checked hike
-	    	var hikeId = $(this).attr('id');
 	    	var json = {"hikeId": hikeId};
-	    	//alert('Id:'+hikeId);
 	    	$.ajax({
 				url: "includes/get-hike.inc.php",
 				type: "POST",
 				data: json,
 				success: function (data) {
-					//alert(data);
+					//Show the checked hike on the map
+					var hike = JSON.parse(data);
+					showHikeOnMap(hike);
+
 				},
 				error: function(xhr, ajaxOptions, thrownError){
 					alert("Error");
 				},
-				timeout: 15000//timeout of the ajax call
+				timeout: 15000 //timeout of the ajax call
 			});
 	    } else {
 	    	// Hide the unchecked hike
+	    	removeHikeFromMap(hikeId);
 	    }
 	});
+
+	function removeHikeFromMap(hikeId) {
+		//Remove the hike from the map
+		layer.eachLayer(function (layerInGroup) {
+		    if (layerInGroup.id === hikeId) {
+		    	layer.removeLayer(layerInGroup);
+		    }// it's the marker
+		});
+	}
+
+	function showHikeOnMap(hike) {
+		var id = hike.id;
+		var title = hike.title;
+    	var name = hike.name;
+    	var participants = hike.participants;
+    	var weather = hike.weather;
+    	var description = hike.description;
+    	var startdate = hike.startdate;
+    	var dateStart = new Date(Number(startdate));
+    	var enddate = hike.enddate;
+    	var dateEnd = new Date(Number(enddate));
+    	var mapfile = hike.mapfile;
+    	var distance = hike.distance;
+    	var userId = hike.userId;
+    	var localId = hike.localId;
+    	var observationPoints = hike.observationPoints;
+    	var track = hike.track;
+
+		mapItems = [];
+
+		//Decode observation points
+		var jsonObservationPoints = JSON.parse(observationPoints);
+		var totalSheepCount=0;
+		for (var i = 0; i < jsonObservationPoints.length; i++) {
+			var latitude = Number(jsonObservationPoints[i].locationPoint.mLatitude);
+			var longitude = Number(jsonObservationPoints[i].locationPoint.mLongitude);
+			var pointParent = new L.LatLng(latitude, longitude);
+			var date = new Date(Number(jsonObservationPoints[i].timeOfObservationPoint));
+			var marker = L.marker(pointParent, {icon: redIcon});
+			marker.bindPopup("<b>Observasjonspunkt "+jsonObservationPoints[i].pointId+"</b><br>Kl. "+date.format("HH:MM")+"<br>Sau sett: "+jsonObservationPoints[i].sheepCount);
+			mapItems.push(marker);
+			totalSheepCount+=Number(jsonObservationPoints[i].sheepCount);
+			//Decode observations
+			var observationList = jsonObservationPoints[i].observationList;
+			for (var j = 0; j < observationList.length; j++) {
+				var latitude = Number(observationList[j].locationObservation.mLatitude);
+				var longitude = Number(observationList[j].locationObservation.mLongitude);
+				var pointChild = new L.LatLng(latitude, longitude);
+				var marker = L.marker(pointChild, {icon: blueIcon});
+				mapItems.push(marker);
+				marker.bindPopup("<b>Observasjon "+observationList[j].observationId+"</b><br>Type: "+observationList[j].typeOfObservation+"<br>Antall: "+observationList[j].sheepCount);
+				// Add polyline between observation point and observation
+				var line = new L.Polyline([pointParent,pointChild], {
+				    color: 'blue',
+				    weight: 3,
+				    opacity: 0.8,
+				    smoothFactor: 1
+				});
+				mapItems.push(line);
+			}
+		}
+
+		//Decode track
+    	trackPointList = [];
+    	var jsonTrack = JSON.parse(track);
+    	for (var i = 0; i < jsonTrack.length; i++) {
+    		var latitude = Number(jsonTrack[i].mLatitude);
+			var longitude = Number(jsonTrack[i].mLongitude);
+			var point = new L.LatLng(latitude, longitude);
+			trackPointList.push(point);
+    	}
+    	
+    	var markerStart = L.marker(trackPointList[0], {icon: startIcon});
+		markerStart.bindPopup("<b>Start</b> "+dateStart.format("HH:MM"));
+		mapItems.push(markerStart);
+		var markerEnd = L.marker(trackPointList[trackPointList.length-1], {icon: stopIcon});
+		markerEnd.bindPopup("<b>Slutt</b> "+dateEnd.format("HH:MM"));
+		mapItems.push(markerEnd);
+
+    	var trackPolyline = new L.Polyline(trackPointList, {
+		    color: 'red',
+		    weight: 4,
+		    opacity: 0.8,
+		    smoothFactor: 1
+		}).bindPopup('<b>'+title+'</b><br>'+dateStart.format("dd/mm/yyyy HH:MM")+'-'+dateEnd.format('HH:MM')+'<br><b>Gjeter:</b> '+name+'<br><b>Deltakere:</b> '+participants+'<br><b>Antall sau sett:</b> '+totalSheepCount+'<br><b>Vær:</b> '+weather+'<br><b>Distanse:</b> '+distance+'<br><b>Detaljer:</b> '+description);
+		mapItems.push(trackPolyline);
+		mymap.fitBounds(trackPolyline.getBounds());
+
+		//Add all map items to the map
+		var layer2 = L.layerGroup(mapItems);
+		layer2.id = id;
+		layer.addLayer(layer2);
+	}
 
 </script>
 </html>
