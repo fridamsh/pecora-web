748bcf4927c6b8e164780836c265389b478e34db
fridamsh <fridas@stud.ntnu.no>
2018-03-12 15:29:30 +0100

    La til sortering på startdate når man lager lista over turene i datoer

--- includes/get-data.inc.php
+++ includes/get-data.inc.php
@@ -6,7 +6,7 @@ include '../models/Hike.php';
 
 $userId = $_SESSION['u_id'];
 
-$sql = "SELECT * FROM hike WHERE userId=$userId;";
+$sql = "SELECT * FROM hike WHERE userId=$userId ORDER BY startdate DESC;";
 $result = mysqli_query($conn, $sql);
 $resultCheck = mysqli_num_rows($result);
 
@@ -16,18 +16,18 @@ if ($resultCheck > 0) {
 		$hike = new Hike();
 		$hike->set_id($row['id']);
 		$hike->set_title($row['title']);
-		$hike->set_name($row['name']);
-		$hike->set_participants($row['participants']);
-		$hike->set_weather($row['weather']);
-		$hike->set_description($row['description']);
+		//$hike->set_name($row['name']);
+		//$hike->set_participants($row['participants']);
+		//$hike->set_weather($row['weather']);
+		//$hike->set_description($row['description']);
 		$hike->set_startdate($row['startdate']);
-		$hike->set_enddate($row['enddate']);
-		$hike->set_mapfile($row['mapfile']);
-		$hike->set_distance($row['distance']);
-		$hike->set_observationPoints($row['observationPoints']);
-		$hike->set_track($row['track']);
-		$hike->set_userId($row['userId']);
-		$hike->set_localId($row['localId']);
+		//$hike->set_enddate($row['enddate']);
+		//$hike->set_mapfile($row['mapfile']);
+		//$hike->set_distance($row['distance']);
+		//$hike->set_observationPoints($row['observationPoints']);
+		//$hike->set_track($row['track']);
+		//$hike->set_userId($row['userId']);
+		//$hike->set_localId($row['localId']);
 		array_push($hikes, $hike);
 	}
 	echo json_encode($hikes);
