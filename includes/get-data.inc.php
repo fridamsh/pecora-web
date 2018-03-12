66eaf126cb15666cb709b88add2be1a6a7c5bb30
fridamsh <fridas@stud.ntnu.no>
2018-03-12 19:23:51 +0100

    Fjernet kommentarer

--- includes/get-data.inc.php
+++ includes/get-data.inc.php
@@ -16,18 +16,7 @@ if ($resultCheck > 0) {
 		$hike = new Hike();
 		$hike->set_id($row['id']);
 		$hike->set_title($row['title']);
-		//$hike->set_name($row['name']);
-		//$hike->set_participants($row['participants']);
-		//$hike->set_weather($row['weather']);
-		//$hike->set_description($row['description']);
 		$hike->set_startdate($row['startdate']);
-		//$hike->set_enddate($row['enddate']);
-		//$hike->set_mapfile($row['mapfile']);
-		//$hike->set_distance($row['distance']);
-		//$hike->set_observationPoints($row['observationPoints']);
-		//$hike->set_track($row['track']);
-		//$hike->set_userId($row['userId']);
-		//$hike->set_localId($row['localId']);
 		array_push($hikes, $hike);
 	}
 	echo json_encode($hikes);
