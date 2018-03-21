bfedd62155957415d22da754e6bbf7a6cfedabf8
fridamsh <fridas@stud.ntnu.no>
2018-03-21 12:20:34 +0100

    Nå fungerer bulk synking av turer

--- app/insertHikes.php
+++ app/insertHikes.php
@@ -5,52 +5,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	$hikes = json_decode($_POST["hikes"]);
 
-	for ($i = 0; $i < length($hikes); $i++) {
-		$title = $hikes[0]->title;
-		$name = $hikes[0]->name;	
-		$participants = $hikes[0]->participants;	
-		$weather = $hikes[0]->weather;	
-		$description = $hikes[0]->description;	
-		$startdate = $hikes[0]->startdate;	
-		$enddate = $hikes[0]->enddate;	
-		$mapfile = $hikes[0]->mapfile;	
-		$distance = $hikes[0]->distance;	
-		$observationPoints = $hikes[0]->observationPoints;	
-		$track = $hikes[0]->track;	
-		$userId = $hikes[0]->userId;	
-		$localId = $hikes[0]->localId;
+	$syncedHikes = 0;
+	$alreadySyncedHikes = 0;
+	$errors = 0;
 
-		$syncedHikes = 0;
-		$alreadySyncedHikes = 0;
-		$errors = 0;
+	for ($i = 0; $i < sizeof($hikes); $i++) {
+		$title = $hikes[$i]->title;
+		$name = $hikes[$i]->name;
+		$participants = $hikes[$i]->numberOfParticipants;	
+		$weather = $hikes[$i]->weatherState;	
+		$description = $hikes[$i]->description;	
+		$startdate = $hikes[$i]->dateStart;	
+		$enddate = $hikes[$i]->dateEnd;	
+		$mapfile = $hikes[$i]->mapFileName;	
+		$distance = $hikes[$i]->distance;	
+		$observationPoints = json_encode($hikes[$i]->observationPoints, JSON_UNESCAPED_UNICODE);	
+		$track = json_encode($hikes[$i]->trackPoints);	
+		$userId = $hikes[$i]->userId;
+		$localId = $hikes[$i]->id;
 		
 		$sql = "SELECT * FROM hike WHERE userId='$userId' AND localId='$localId';";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
 			$alreadySyncedHikes++;
-			//$json['error'] = 'Turen er allerede synkronisert';
-			//echo json_encode($json);
-			//mysqli_close($conn);
 		} else {
 			$sql = "INSERT INTO hike (title, name, participants, weather, description, startdate, enddate, mapfile, distance, observationPoints, track, userId, localId) VALUES ('$title', '$name', '$participants', '$weather', '$description', '$startdate', '$enddate', '$mapfile', '$distance', '$observationPoints', '$track', '$userId', '$localId');";
 			$result = mysqli_query($conn, $sql);
 			
 			if ($result == 1 ) {
 				$syncedHikes++;
-				//$json['success'] = 'Tur synkronisert';
 			} else {
 				$errors++;
-				//$json['error'] = 'Det skjedde en feil ved synkronisering';
 			}
-			//echo json_encode($json);
-			//mysqli_close($conn);
-		}	
+		}
 	}
 	if ($errors > 0) {
-		$json['error'] = $errors + ' feil, ' + $syncedHikes + ' synket og ' + $alreadySyncedHikes + ' allerede synket';
+		$json['error'] = $errors . ' feil, ' . $syncedHikes . ' synket og ' . $alreadySyncedHikes . ' allerede synket';
 	} else {
-		$json['success'] = $syncedHikes + ' synket, ' + $alreadySyncedHikes + ' allerede synket og 0 feil';
+		$json['success'] = $syncedHikes . ' synket, ' . $alreadySyncedHikes . ' allerede synket og ' . $errors . ' feil';
 	}
 	echo json_encode($json);
 	mysqli_close($conn);
